8853d06f773426234515886bbf60f22cc20b1950
Anna Erkers <31205429+Hilwynn@users.noreply.github.com>
2018-11-12 17:13:11 +0100

    Created LandingPage

---
+++ frontend/src/containers/LandingPage.js
@@ -0,0 +1,13 @@
+import React, { Component } from "react"
+
+class LandingPage extends Component {
+  render() {
+    return (
+      <div className="wrapper">
+        <h1>Welcome</h1>
+      </div>
+    )
+  }
+}
+
+export default LandingPage
