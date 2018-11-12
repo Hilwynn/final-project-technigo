4473f2a13cd139695f55a9234bbd3451d4607411
Anna Erkers <31205429+Hilwynn@users.noreply.github.com>
2018-11-12 17:14:15 +0100

    Added basic routing

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,11 +1,22 @@
 import React, { Component } from "react"
+import { Route, Switch, withRouter } from "react-router-dom"
 import "./App.scss"
+import LandingPage from "./containers/LandingPage.js"
 
 class App extends Component {
   render() {
     return (
-      <div className="wrapper">
-        Hello
+      <div>
+        Header
+        <Switch>
+          <Route
+            path="/"
+            exact
+            render={() => (
+              <LandingPage />
+            )
+            } />
+        </Switch>
       </div>
     )
   }
--- frontend/src/index.js
+++ frontend/src/index.js
@@ -1,6 +1,12 @@
 import React from "react"
 import ReactDOM from "react-dom"
-import "./index.scss"
+import { BrowserRouter as Router } from "react-router-dom"
 import App from "./App"
+import "./index.scss"
 
-ReactDOM.render(<App />, document.getElementById("root"))
+ReactDOM.render(
+  <Router onUpdate={() => window.scrollTo(0, 0)}>
+    <App />
+  </Router>,
+  document.getElementById("root")
+)
