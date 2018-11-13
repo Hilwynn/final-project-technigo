c02a9d432d847ae51919dbc33e273b808ff18fba
Anna Erkers <31205429+Hilwynn@users.noreply.github.com>
2018-11-13 23:00:40 +0100

    Added CharacterSheet component

---
+++ src/components/CharacterSheet.js
@@ -0,0 +1,13 @@
+import React, { Component } from "react"
+import "./CharacterSheet.scss"
+
+class CharacterSheet extends Component {
+  render() {
+    return (
+      <div className="wrapper">
+      </div>
+    )
+  }
+}
+
+export default CharacterSheet
